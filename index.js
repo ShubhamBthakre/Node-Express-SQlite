098423572c0bfe0f5566238367db963c842babee
Shubham Thakre <132575491+ShubhamBthakre@users.noreply.github.com>
2023-11-05 23:39:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -148,3 +148,25 @@ app.get("/authors/:authorId/books/", async (request, response) => {
   const booksArray = await db.all(getAuthorBooksQuery);
   response.send(booksArray);
 });
+
+// Filtering GET Books API
+app.get("/books/", async (request, response) => {
+  const {
+    offset = 2,
+    limit = 5,
+    order = "ASC",
+    order_by = "book_id",
+    search_q = "",
+  } = request.query;
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+     book
+    WHERE
+     title LIKE '%${search_q}%'
+    ORDER BY ${order_by} ${order}
+    LIMIT ${limit} OFFSET ${offset};`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
